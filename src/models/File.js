0725b3910f095717adc6dd475d8356631766a1ac
Victor Rodrigues <vr33209@gmail.com>
2019-04-18 13:09:36 -0300

    adicionando variavel de ambientes

--- src/models/File.js
+++ src/models/File.js
@@ -15,9 +15,10 @@ const File = new mongoose.Schema({
     toObject: { virtuals: true },
     toJSON: { virtuals: true }
   });
+const url = process.env.URL || 'http://localhost:3333';
 
 File.virtual('url').get(function () {
-  return `http://localhost:3333/files/${encodeURIComponent(this.path)}`
+  return `${url}/files/${encodeURIComponent(this.path)}`
 
 })
 
